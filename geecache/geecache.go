b1a3f80ab1f4dd32d67a8e6234cd4341b1781988
maywzh <maywzh@gmail.com>
2020-09-02 18:19:13 +0800

    fix

--- geecache/geecache.go
+++ geecache/geecache.go
@@ -28,6 +28,7 @@ type Getter interface {
 type GetterFunc func(key string) ([]byte, error)
 
 // Get implements Getter interface function
+// 定义一个函数类型 F，并且实现接口 A 的方法，然后在这个方法中调用自己。这是 Go 语言中将其他函数（参数返回值定义与 F 一致）转换为接口 A 的常用技巧。
 func (f GetterFunc) Get(key string) ([]byte, error) {
 	return f(key)
 }
@@ -97,7 +98,6 @@ func (g *Group) load(key string) (value ByteView, err error) {
 				log.Println("[GeeCache] Failed to get from peer", err)
 			}
 		}
-
 		return g.getLocally(key)
 	})
 
